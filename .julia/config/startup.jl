381941e4787372ca64aa769d67883233d495f5db
Fredrik Ekre <ekrefredrik@gmail.com>
2022-10-06 12:55:15 +0200

    [julia] Update startup.jl to define REPL as local variable.

--- .julia/config/startup.jl
+++ .julia/config/startup.jl
@@ -1,5 +1,5 @@
 if Base.isinteractive() &&
-   (REPL = get(Base.loaded_modules, Base.PkgId(Base.UUID("3fa0cd96-eef1-5676-8a61-b3b8758bbffb"), "REPL"), nothing); REPL !== nothing)
+   (local REPL = get(Base.loaded_modules, Base.PkgId(Base.UUID("3fa0cd96-eef1-5676-8a61-b3b8758bbffb"), "REPL"), nothing); REPL !== nothing)
 
     # Exit Julia with :q
     pushfirst!(REPL.repl_ast_transforms, function(ast::Union{Expr,Nothing})
