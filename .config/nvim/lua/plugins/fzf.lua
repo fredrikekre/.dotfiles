c1e4547b2111da1cd5148d9cf17b79d3d4310806
Fredrik Ekre <ekrefredrik@gmail.com>
2024-07-02 16:52:34 +0200

    [nvim] Update to fzf 0.53.0

--- .config/nvim/lua/plugins/fzf.lua
+++ .config/nvim/lua/plugins/fzf.lua
@@ -4,13 +4,12 @@
 
 return {
     "junegunn/fzf.vim",
-    commit = "1e054c1d075d87903647db9320116d360eb8b024",
     config = function()
         vim.keymap.set("n", "<C-F>", ":Rg!<CR>")
         -- TODO: Use vim.keymap.set
         vim.cmd("nnoremap <expr> <C-p> len(system('git rev-parse')) ? ':Files!<CR>' : ':GFiles!<CR>'")
     end,
     dependencies = {
-        {"junegunn/fzf", commit = "952b6af4454ed55626d78e3845c6b5b640ac831d"},
+        {"junegunn/fzf"},
     },
 }
