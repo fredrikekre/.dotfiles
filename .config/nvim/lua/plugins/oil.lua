76dc30fa329af1cc68144c514e65a26af4d129fa
Fredrik Ekre <ekrefredrik@gmail.com>
2024-07-03 11:13:00 +0200

    [nvim] Add oil.nvim

---
+++ .config/nvim/lua/plugins/oil.lua
@@ -0,0 +1,17 @@
+-- https://github.com/stevearc/oil.nvim
+return {
+    "stevearc/oil.nvim",
+    opts = {
+        columns = {
+            -- "icon",
+            -- "permissions",
+            -- "size",
+            -- "mtime",
+        },
+        delete_to_trash = true,
+        skip_confirm_for_simple_edits = true,
+        view_options = {
+            show_hidden = true,
+        },
+    },
+}
