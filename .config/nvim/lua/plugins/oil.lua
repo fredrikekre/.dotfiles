42233458ec19d7bb1996ca367fa6a5c4503d564d
Fredrik Ekre <ekrefredrik@gmail.com>
2024-07-03 12:25:47 +0200

    [nvim] Configuration for oil.nvim
    - Enable icons with nvim-web-devicons
    - Disable some conflicting keymappings
    - Toggle for detailed view

--- .config/nvim/lua/plugins/oil.lua
+++ .config/nvim/lua/plugins/oil.lua
@@ -1,17 +1,37 @@
 -- https://github.com/stevearc/oil.nvim
+
+-- For toggling detailed view
+local detail = false
+
 return {
     "stevearc/oil.nvim",
+    dependencies = {
+        {"nvim-tree/nvim-web-devicons"},
+    },
     opts = {
         columns = {
-            -- "icon",
-            -- "permissions",
-            -- "size",
-            -- "mtime",
+            "icon",
         },
         delete_to_trash = true,
         skip_confirm_for_simple_edits = true,
         view_options = {
             show_hidden = true,
+        },
+        keymaps = {
+            -- Disable these since they are used for navigation
+            ["<C-h>"] = false,
+            ["<C-l>"] = false,
+            -- Toggle detailed view with <leader>d
+            ["<leader>d"] = {
+                callback = function()
+                    detail = not detail
+                    local cols = {"icon"}
+                    if detail then
+                        cols = {"icon", "permissions", "size", "mtime"}
+                    end
+                    require("oil").set_columns(cols)
+                end,
+            },
         },
     },
 }
