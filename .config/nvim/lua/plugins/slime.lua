11d62623316c6df828d502197c41aa5524d62709
Fredrik Ekre <ekrefredrik@gmail.com>
2024-08-05 09:10:14 +0200

    [nvim] Reset cursor after slime region send

--- .config/nvim/lua/plugins/slime.lua
+++ .config/nvim/lua/plugins/slime.lua
@@ -12,12 +12,23 @@ vim.cmd([[
 ]]
 )
 
+local function slime_send_region()
+    local keys = ":<C-u>call slime#send_op(visualmode(), 1)<CR>"
+    local mode = "x"
+    vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(keys, true, true, true), mode, true)
+    return
+end
+
 return {
     "jpalardy/vim-slime",
     config = function()
         vim.g["slime_target"] = "tmux"
         vim.g["slime_bracketed_paste"] = 1
         vim.keymap.set("n", "<S-CR>", "<Plug>SlimeParagraphSend")
-        vim.keymap.set("x", "<S-CR>", "<Plug>SlimeRegionSend")
+        vim.keymap.set("x", "<S-CR>", function()
+            local loc = vim.api.nvim_win_get_cursor(0)
+            slime_send_region()
+            vim.api.nvim_win_set_cursor(0, loc)
+        end)
     end,
 }
