da17299d4e62f0e4b60acb66f8d1c6038b2231db
Fredrik Ekre <ekrefredrik@gmail.com>
2025-04-14 14:22:47 +0200

    [nvim] Update conform config

--- .config/nvim/lua/plugins/conform.lua
+++ .config/nvim/lua/plugins/conform.lua
@@ -3,17 +3,29 @@
 local function configure_conform()
     local conform = require("conform")
     -- Keymaps
-    vim.keymap.set({"n", "v"}, "<leader>f", function() conform.format({}) end, {silent = true})
+    vim.keymap.set(
+        {"n", "v"},
+        "<leader>f",
+        function()
+            conform.format(
+            {},
+            function(err)
+                if not err then
+                    local mode = vim.api.nvim_get_mode().mode
+                    if vim.startswith(string.lower(mode), "v") then
+                        vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Esc>", true, false, true), "n", true)
+                    end
+                end
+            end
+            )
+        end,
+        {silent = true}
+    )
     -- vim.o.formatexpr = "v:lua.require('conform').formatexpr()"
     -- Configure conform
     local opts = {
         formatters_by_ft = {
             julia = {"runic"},
-        },
-        formatters = {
-            runic = {
-                command = "runic",
-            },
         },
         default_format_opts = {
             timeout_ms = 10000,
@@ -24,5 +36,6 @@ end
 
 return {
     "stevearc/conform.nvim",
+    commit = "db8a4a9edb217067b1d7a2e0362c74bfe9cc944d",
     config = configure_conform,
 }
